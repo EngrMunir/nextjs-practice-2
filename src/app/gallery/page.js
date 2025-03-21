9a3e4edb7358200a604788bccda66296c3ba2704
munir <smsirajulmonir@gmail.com>
2025-03-21 17:42:06 +0600

    metadata and font optimize explored

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,12 +1,15 @@
 import Image from 'next/image';
-import React from 'react';
+import nextImg from "../../assets/car.jpg"
 
 const GalleryPage = () => {
     return (
         <div className='text-center'>
+            <h1>Regular Image</h1>
             <img src='https://i.ibb.co.com/3xBMb3P/car.jpg' alt='' className='mx-auto' width={400} height={400}/>
-            <h1>Nextjs Image Tag</h1>
+            <h1>Nextjs Image</h1>
             <Image src="https://i.ibb.co.com/3xBMb3P/car.jpg" width={400} height={400} alt='image' className='mx-auto'/>
+            <h1>Local Image</h1>
+            <Image src={nextImg} width={400} height={400} alt='image' className='mx-auto'/>
         </div>
     );
 };
